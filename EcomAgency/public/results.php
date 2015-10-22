2f8db56945aea4e21da1b4c1a6a76e87150ec3b5
Edwinamunga <billingtonamunga@gmail.com>
2015-10-22 11:55:34 +0300

    Modified folder structure

---
+++ EcomAgency/public/results.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Aqutech systems, web development, mobile apps, kenyan developers" >    
+        <meta name="author" content="Edwin Amakobe">
+
+        <title>Ecom</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Custom CSS -->
+        <link href="css/landing-page.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+            <div class="container topnav">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="w-nav-brand brand-link" href="#home" style="padding:0px 0px 10px 16px;">
+                        <img height="70" style="" src="" alt="Ecom agency logo"></a></div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#services" data-toggle="dropdown" class="dropdown-toggle">Quotes<b class="caret"></b></a>
+
+
+                        </li>
+
+                        <li>
+                            <a href="#projects" data-toggle="dropdown" class="dropdown-toggle">Policies<b class="caret"></b></a>
+
+                        </li>
+
+                        <li>                        
+                            <a href="#" data-toggle="dropdown" class="dropdown-toggle">Claims<b class="caret"></b></a>
+
+                        </li>
+
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+        <br>
+        <br>
+        <br>
+        <br>
+        
+            <?php
+            include ('db_config.php');
+            $response = array();
+            //$category = "motor";
+            //$type = "personal";
+            //$amount = 4.80;
+            if (isset($_POST['type']) && isset($_POST['category']) && isset($_POST['percentage'])) {
+            //if($category){
+            //check string
+            //posting the categories
+                $type = $_POST['type'];
+                $category = $_POST['category'];
+                $percentage = $_POST['percentage'];
+
+                //stripslashes($category);    
+                //mysqli_real_escape_string($category);
+                //search database
+                $query = "select * from products where type='$type' and category = '$category' and percentage<'$percentage' order by percentage ASC";
+                $result = mysqli_query($con, $query);
+
+                //check if query excecuted
+                if ($result) {
+
+                    if ($result->num_rows > 0) { 
+                        echo "<div class=\"row\"><div class=\"btn btn-success col-lg-12\"><p>Total Results Found</p></div></div>";
+
+                        while ($row = $result->fetch_object()) {
+                            echo "<div class=\"col-md-4\">Name: " . $row->name . "<br>".
+                            "Type: " . $row->type . "<br>".
+                            "Category: " . $row->category . "<br>".
+                            "Company: " . $row->company . "<br>".
+                            "Percentage: " . $row->percentage . "% of current value <br>".
+                            "Description: " . $row->description . "<br>".
+                            "Validity: " . $row->validity . "Years". "<br>".
+                            "Last Edited: " . $row->updated_at .                            " <hr>
+                  <button class=\"btn btn-primary form-control \">Buy Quote</button>
+                  <div class=\"panel-footer\"></div>
+            </div>
+            </div>";
+                        }
+                    } else {
+                        echo "<div class=\"row\"><div class=\"btn btn-warning col-lg-12\"><p>Total Results Found</p></div></div>";
+                        echo "There are no results for your search go back and search using different figures";
+                    }
+                } else {
+                    echo "<div class=\"row\"><div class=\"btn btn-danger col-lg-12\"><p>Total Results Found</p></div></div>";
+                    echo "<div class=\"panel-danger\"> <h3>there was a problem searching quotes<h3></div>";
+                }
+            } else {
+
+                echo "<div class=\"panel-danger\"> <h3>Please fill all fields<h3></div>";
+            }
+            ?>
+        
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+</html>
---
+++ EcomAgency/resources/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<<html>
+<head>
+	<title>{{$title}}</title>
+	{{html::style('/css/main.css')}}
+</head>
+<body>
+	<div class="container">
+		<div id="header">
+		</div>
+		<div class="content">
+		</div>
+		<div class="footer">
+		</div>
+	</div>
+
+</body>
+</html>
